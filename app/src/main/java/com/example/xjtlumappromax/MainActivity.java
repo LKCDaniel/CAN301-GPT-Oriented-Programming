06ecc9bf2bd8d5367e0a92beab00f8e54d9535d2
LKCDaniel <18611377374@163.com>
2024-11-29 23:00:19 +0800

    把binding重新弄好了，页面切换恢复正常

--- app/src/main/java/com/example/xjtlumappromax/MainActivity.java
+++ app/src/main/java/com/example/xjtlumappromax/MainActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.xjtlumappromax.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     private static final String TAG = "com.example.xjtlumappromax.MainActivity";
     private DatabaseHelper dbHelper;
     private SQLiteDatabase db;
@@ -18,7 +26,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_map, R.id.navigation_chat, R.id.navigation_friends, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
 
         dbHelper = new DatabaseHelper(this);
 
@@ -47,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     @Override
